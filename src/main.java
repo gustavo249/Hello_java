101ab4b186f73ccafa92013346fa0705a397d8b9
Gustavo <theworldismine249@yahoo.com>
2016-02-10 00:43:16 +0200

    refresh data

--- src/main.java
+++ src/main.java
@@ -3,7 +3,11 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello my friend !");
-        System.out.println("Java is interesting !");
+        int n = 2;
+        byte c = 5;
+        short s = 4;
+        long l = 5000L + 10 * (n + c + s);
+        System.out.println("long total = " + l);
     }
 }
++++--
